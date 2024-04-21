01f9534e0d68fe2ba7561a11cba7f108ac3b6a36
Arty <belovaa@mer.ci.nsu.ru>
2024-04-21 22:52:55 +0700

    Add project files.

---
+++ MatrixMultiplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputConsoleAppTesting
+{
+    public static class MatrixMultiplier
+    {
+        public static int[,] GenerateMultiplicationMatrix(int x, int y)
+        {
+            int[,] matrix = new int[x, y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matrix[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputConsoleAppTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа для создания матрицы умножения:");
+            Console.Write("Первое число: ");
+            if (!int.TryParse(Console.ReadLine(), out int x))
+            {
+                Console.WriteLine("Ошибка: Введено некорректное значение для первого числа.");
+                return;
+            }
+
+            Console.Write("Второе число: ");
+            if (!int.TryParse(Console.ReadLine(), out int y))
+            {
+                Console.WriteLine("Ошибка: Введено некорректное значение для второго числа.");
+                return;
+            }
+
+            Console.WriteLine("Матрица умножения:");
+            int[,] multiplicationMatrix = MatrixMultiplier.GenerateMultiplicationMatrix(x, y);
+            PrintMatrix(multiplicationMatrix);
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
